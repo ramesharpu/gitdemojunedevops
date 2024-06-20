241cc110443a8546cb2c417224484e558d6fa026
Ramesh A <ramesharpu@gmail.com>
2024-06-20 21:45:51 +0530

    initial commit

---
+++ src/basic/HelloWorld.java
@@ -0,0 +1,15 @@
+package basic;
+
+public class HelloWorld {
+	
+	public void welcomeMessage() {
+		System.out.println("Hello Everyone, Welcome to DevOps Training!!!");
+	}
+
+	public static void main(String[] args) {
+		HelloWorld obj = new HelloWorld();
+		obj.welcomeMessage();
+
+	}
+
+}
